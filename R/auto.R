7fb9055f24a8d22eeb7f1ba2793b5777b9f5204b
Jonathan Trattner <accounts@jdtrat.com>
2021-02-14 16:20:25 -0500

    Add GitHub links to DESCRIPTION

---
+++ R/auto.R
@@ -0,0 +1,26 @@
+
+#' Create a Github Action that Comments a Reminder on an Issue
+#'
+#' @param issue_number The Github Issue Number for the repo
+#' @param remindee The person to be reminded
+#' @param action What the person needs to be reminded about
+#'
+#' @export
+
+auto_issue_comment <- function(issue_number, remindee, action) {
+
+  data <- list(issue_number = issue_number,
+               remindee = remindee,
+               action = action)
+
+  template <- readLines(system.file("template_comment_reminder.yml", package = "automatr"))
+
+  tmp <- tempdir()
+
+  writeLines(whisker::whisker.render(template, data), paste0(tmp, "/auto_issue_comment.yml"))
+
+  usethis::use_github_action(url = paste0(tmp, "/auto_issue_comment.yml"))
+
+  on.exit(unlink(tmp))
+
+}
